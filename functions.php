22c4d5d676eec62e759239cb000aed015385ecd5
raulfunkie <raulrincon@gmail.com>
2021-05-01 19:58:01 -0400

    fixed emoji declaration

--- functions.php
+++ functions.php
@@ -47,20 +47,7 @@ function remove_width_attribute( $html ) {
    $html = preg_replace( '/(width|height)="\d*"\s/', "", $html );
    return $html;
 }
-
 remove_filter('the_content', 'wpautop');
-
-function disable_wp_emojicons() {
-  remove_action( 'admin_print_styles', 'print_emoji_styles' );
-  remove_action( 'wp_head', 'print_emoji_detection_script', 7 );
-  remove_action( 'admin_print_scripts', 'print_emoji_detection_script' );
-  remove_action( 'wp_print_styles', 'print_emoji_styles' );
-  remove_filter( 'wp_mail', 'wp_staticize_emoji_for_email' );
-  remove_filter( 'the_content_feed', 'wp_staticize_emoji' );
-  remove_filter( 'comment_text_rss', 'wp_staticize_emoji' );
-}
-add_action( 'init', 'disable_wp_emojicons' );
-add_filter( 'emoji_svg_url', '__return_false' );
 
 add_action( 'after_setup_theme', 'theme_functions' );
 function theme_functions() {
