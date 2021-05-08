78bff5ab9632b21b1cc3cb6741ba0d218749e221
raulfunkie <raulrincon@gmail.com>
2021-05-07 22:26:32 -0400

    another attempt at fixing the link scheme

--- includes/related-posts.php
+++ includes/related-posts.php
@@ -1,6 +1,6 @@
 <div class="related">
   <ul>
-    <?php $popularpost = new WP_Query( array( 'posts_per_page' => 4, 'meta_key' => 'wpb_post_views_count', 'orderby' => 'meta_value_num', 'order' => 'RAND'  ) );
+    <?php $popularpost = new WP_Query( array( 'posts_per_page' => 3, 'meta_key' => 'wpb_post_views_count', 'orderby' => 'meta_value_num', 'order' => 'RAND'  ) );
     while ( $popularpost->have_posts() ) : $popularpost->the_post(); ?>
     <li>
       <a href="<?php the_permalink(); ?>" rel="bookmark" <?php the_title_attribute(); ?>>
