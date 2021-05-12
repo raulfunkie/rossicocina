bb361bddd44af578cd735797d2ffc3a5e29d8277
raulfunkie <raulrincon@gmail.com>
2021-05-11 20:37:41 -0400

    updated files and added favico

--- functions.php
+++ functions.php
@@ -139,22 +139,22 @@ function prefetch_links() { ?>
 add_action( 'wp_head', 'prefetch_links', 10 );
 
 function favico() { ?>
-  <link rel="apple-touch-icon" sizes="57x57" href="assets/fav/apple-icon-57x57.png">
-  <link rel="apple-touch-icon" sizes="60x60" href="assets/fav/apple-icon-60x60.png">
-  <link rel="apple-touch-icon" sizes="72x72" href="assets/fav/apple-icon-72x72.png">
-  <link rel="apple-touch-icon" sizes="76x76" href="assets/fav/apple-icon-76x76.png">
-  <link rel="apple-touch-icon" sizes="114x114" href="assets/fav/apple-icon-114x114.png">
-  <link rel="apple-touch-icon" sizes="120x120" href="assets/fav/apple-icon-120x120.png">
-  <link rel="apple-touch-icon" sizes="144x144" href="assets/fav/apple-icon-144x144.png">
-  <link rel="apple-touch-icon" sizes="152x152" href="assets/fav/apple-icon-152x152.png">
-  <link rel="apple-touch-icon" sizes="180x180" href="assets/fav/apple-icon-180x180.png">
-  <link rel="icon" type="image/png" sizes="192x192"  href="assets/fav/android-icon-192x192.png">
-  <link rel="icon" type="image/png" sizes="32x32" href="assets/fav/favicon-32x32.png">
-  <link rel="icon" type="image/png" sizes="96x96" href="assets/fav/favicon-96x96.png">
-  <link rel="icon" type="image/png" sizes="16x16" href="assets/fav/favicon-16x16.png">
-  <link rel="manifest" href="assets/fav/manifest.json">
+  <link rel="apple-touch-icon" sizes="57x57" href="<?php echo get_stylesheet_directory_uri(); ?>/assets/fav/apple-icon-57x57.png">
+  <link rel="apple-touch-icon" sizes="60x60" href="<?php echo get_stylesheet_directory_uri(); ?>/assets/fav/apple-icon-60x60.png">
+  <link rel="apple-touch-icon" sizes="72x72" href="<?php echo get_stylesheet_directory_uri(); ?>/assets/fav/apple-icon-72x72.png">
+  <link rel="apple-touch-icon" sizes="76x76" href="<?php echo get_stylesheet_directory_uri(); ?>/assets/fav/apple-icon-76x76.png">
+  <link rel="apple-touch-icon" sizes="114x114" href="<?php echo get_stylesheet_directory_uri(); ?>/assets/fav/apple-icon-114x114.png">
+  <link rel="apple-touch-icon" sizes="120x120" href="<?php echo get_stylesheet_directory_uri(); ?>/assets/fav/apple-icon-120x120.png">
+  <link rel="apple-touch-icon" sizes="144x144" href="<?php echo get_stylesheet_directory_uri(); ?>/assets/fav/apple-icon-144x144.png">
+  <link rel="apple-touch-icon" sizes="152x152" href="<?php echo get_stylesheet_directory_uri(); ?>/assets/fav/apple-icon-152x152.png">
+  <link rel="apple-touch-icon" sizes="180x180" href="<?php echo get_stylesheet_directory_uri(); ?>/assets/fav/apple-icon-180x180.png">
+  <link rel="icon" type="image/png" sizes="192x192"  href="<?php echo get_stylesheet_directory_uri(); ?>/assets/fav/android-icon-192x192.png">
+  <link rel="icon" type="image/png" sizes="32x32" href="<?php echo get_stylesheet_directory_uri(); ?>/assets/fav/favicon-32x32.png">
+  <link rel="icon" type="image/png" sizes="96x96" href="<?php echo get_stylesheet_directory_uri(); ?>/assets/fav/favicon-96x96.png">
+  <link rel="icon" type="image/png" sizes="16x16" href="<?php echo get_stylesheet_directory_uri(); ?>/assets/fav/favicon-16x16.png">
+  <link rel="manifest" href="<?php echo get_stylesheet_directory_uri(); ?>/assets/fav/manifest.json">
   <meta name="msapplication-TileColor" content="#ffffff">
-  <meta name="msapplication-TileImage" content="assets/fav/ms-icon-144x144.png">
+  <meta name="msapplication-TileImage" content="<?php echo get_stylesheet_directory_uri(); ?>/assets/fav/ms-icon-144x144.png">
   <meta name="theme-color" content="#ffffff">
 <?php }
 add_action( 'wp_head', 'favico', 10 );
