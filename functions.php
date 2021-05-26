fa364f399cd493cad3a2c454aa5fe558afd517f5
raulfunkie <raulrincon@gmail.com>
2021-05-25 22:47:55 -0400

    updated description tab

--- functions.php
+++ functions.php
@@ -383,7 +383,7 @@ if ( ! current_user_can( 'manage_options' ) ) {
 
 add_filter( 'woocommerce_product_tabs', 'woo_remove_product_tabs', 98 );
 function woo_remove_product_tabs( $tabs ) {
-    unset( $tabs['description'] );      	// Remove the description tab
+    // unset( $tabs['description'] );      	// Remove the description tab
     unset( $tabs['reviews'] ); 			// Remove the reviews tab
     unset( $tabs['additional_information'] );  	// Remove the additional information tab
     return $tabs;
