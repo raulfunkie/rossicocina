f2ce4a3a1b1895dadf0160b20f4ec5ffe2be0119
raulfunkie <raulrincon@gmail.com>
2021-08-14 20:41:42 -0400

    updated buttons for quantity add

--- functions.php
+++ functions.php
@@ -436,3 +436,50 @@ function bbloomer_remove_reviews_tab( $tabs ) {
    unset( $tabs['additional_information'] );
    return $tabs;
 }
+ 
+add_action( 'woocommerce_after_quantity_input_field', 'bbloomer_display_quantity_plus' );
+  
+function bbloomer_display_quantity_plus() {
+   echo '<button type="button" class="plus">+</button>';
+}
+  
+add_action( 'woocommerce_before_quantity_input_field', 'bbloomer_display_quantity_minus' );
+  
+function bbloomer_display_quantity_minus() {
+   echo '<button type="button" class="minus">-</button>';
+}
+
+add_action( 'wp_footer', 'bbloomer_add_cart_quantity_plus_minus' );
+  
+function bbloomer_add_cart_quantity_plus_minus() {
+ 
+   if ( ! is_product() && ! is_cart() ) return;
+    
+   wc_enqueue_js( "   
+           
+      $('form.cart,form.woocommerce-cart-form').on( 'click', 'button.plus, button.minus', function() {
+  
+         var qty = $( this ).parent( '.quantity' ).find( '.qty' );
+         var val = parseFloat(qty.val());
+         var max = parseFloat(qty.attr( 'max' ));
+         var min = parseFloat(qty.attr( 'min' ));
+         var step = parseFloat(qty.attr( 'step' ));
+ 
+         if ( $( this ).is( '.plus' ) ) {
+            if ( max && ( max <= val ) ) {
+               qty.val( max );
+            } else {
+               qty.val( val + step );
+            }
+         } else {
+            if ( min && ( min >= val ) ) {
+               qty.val( min );
+            } else if ( val > 1 ) {
+               qty.val( val - step );
+            }
+         }
+ 
+      });
+        
+   " );
+}
